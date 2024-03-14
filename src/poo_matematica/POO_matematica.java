06d8749b5fda7f3c5c530ca34def40551514e40a
Alisson-angeles <2410516@cesba-queretaro.edu.mx>
2024-03-14 08:52:00 -0600

    Mi primer commit

---
+++ src/poo_matematica/POO_matematica.java
@@ -0,0 +1,56 @@
+package poo_matematica;
+
+
+public class POO_matematica {
+
+    public static void main(String[] args) {
+        //Realizar un programa con sw que pueda realizar una de las 9 funciones
+        
+        
+       /*double aleatorio=Math.random();
+       System.out.println("Numero aleatorio entre 0 y 1:"+aleatorio);*/
+        
+        /* double angulo=Math.toRadians(45);
+        double seno= Math.sin(angulo);
+        double coseno=Math.cos(angulo);
+        double tangente=Math.tan(angulo);
+        System.out.println("Seno de 45 grados:"+seno);
+        System.out.println("Coseno de 45 grado:"+coseno);
+        System.out.println("Tangente:"+tangente);*/
+        
+        /*double numero=27;
+        double resultado= Math.cbrt(numero);
+        System.out.println("La raiz cubica del numero"+numero+"es:"+resultado);*/
+        
+        /*double numero=25;
+        double resultado= Math.sqrt(numero);
+        System.out.println("La raiz al cuadrado"+numero+"es:"+resultado);*/
+        
+        /*double base=5;
+        double exponente=3;
+        double resultado=Math.pow(base, exponente);
+        System.out.println("El numero"+base+"elevado a la potencia"+exponente+"es:"+ resultado);*/
+        /*double numero=5.8;
+        long resultado=Math.round(numero);
+        System.out.println("Redondeo numero mas cercano"+numero+":"+resultado);*/
+        
+        
+        /*double numero=5.7;
+        double resultado= Math.floor(numero);
+        System.out.println("Redondeo hacia abajo el numero"+numero+":"+resultado);*/
+        
+       /* double numero = 5.3
+        double resultado = Math.ceil(numero);
+        System.out.println("Redondeonhacia arriba el numero"+numero+":"+ resultado);*/
+
+      /*double radio= 5.0;
+      double area= Math.PI*Math.pow(radio, 2);
+      System.out.println("El area del circulo con radio"+radio + "es" + area);
+      
+      System.out.println(Math.PI);*/
+    }
+}
+
+
+
+
